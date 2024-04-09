806c9636279cc47927a3b39796e2ee8286e0d7ca
RumiAxolotl <rumiaxolotl@gmail.com>
2024-04-09 12:31:06 +0700

    update hyprlock and setup script

--- setup.sh
+++ setup.sh
@@ -1,52 +1,111 @@
 #!/bin/bash
+# Define variables
+GREEN="$(tput setaf 2)[OK]$(tput sgr0)"
+RED="$(tput setaf 1)[ERROR]$(tput sgr0)"
+YELLOW="$(tput setaf 3)[NOTE]$(tput sgr0)"
+CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+LOG="install.log"
 
+# Set the script to exit on error
+set -e
+
+printf "$(tput setaf 2) Welcome to the Arch Linux YAY Hyprland installer!\n $(tput sgr0)"
+
+sleep 2
+
+printf "$YELLOW PLEASE BACKUP YOUR FILES BEFORE PROCEEDING!
+This script will overwrite some of your configs and files!"
+
+sleep 2
+
+printf "\n
+$YELLOW  Some commands requires you to enter your password inorder to execute
+If you are worried about entering your password, you can cancel the script now with CTRL Q or CTRL C and review contents of this script. \n"
+
+sleep 3
 #### Check for yay ####
 ISYAY=/sbin/yay
-if [ -f "$ISYAY" ]; then 
-    echo -e "$COK - yay was located, moving on."
-    yay -Suy
+if [ -f "$ISyay" ]; then
+    printf "\n%s - yay was located, moving on.\n" "$GREEN"
 else 
-    echo -e "$CWR - Yay was NOT located"
-    read -n1 -rep $'[\e[1;33mACTION\e[0m] - Would you like to install yay (y,n) ' INSTYAY
-    if [[ $INSTYAY == "Y" || $INSTYAY == "y" ]]; then
-        git clone https://aur.archlinux.org/yay.git &>> $INSTLOG
+    printf "\n%s - yay was NOT located\n" "$YELLOW"
+    read -n1 -rep "${CAT} Would you like to install yay (y,n)" INST
+    if [[ $INST =~ ^[Yy]$ ]]; then
+        git clone https://aur.archlinux.org/yay.git
         cd yay
-        makepkg -si --noconfirm &>> ../$INSTLOG
+        makepkg -si --noconfirm 2>&1 | tee -a $LOG
         cd ..
-        
     else
-        echo -e "$CER - Yay is required for this script, now exiting"
+        printf "%s - yay is required for this script, now exiting\n" "$RED"
         exit
     fi
+# update system before proceed
+    printf "${YELLOW} System Update to avoid issue\n" 
+    yay -Syu --noconfirm 2>&1 | tee -a $LOG
 fi
+
+# Function to print error messages
+print_error() {
+    printf " %s%s\n" "$RED" "$1" "$NC" >&2
+}
+
+# Function to print success messages
+print_success() {
+    printf "%s%s%s\n" "$GREEN" "$1" "$NC"
+}
+
+
+
+
+
 
 ### Install all of the above pacakges ####
 read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+echo
+
+if [[ $inst =~ ^[Nn]$ ]]; then
+    printf "${YELLOW} No packages installed. Goodbye! \n"
+            exit 1
+        fi
+
 if [[ $INST == "Y" || $INST == "y" ]]; then
-    yay -R --noconfirm swaylock waybar
-    yay -S --noconfirm hyprland polkit-gnome ffmpeg neovim viewnior rofi      \
-pavucontrol thunar starship wl-clipboard wf-recorder swww   \
-grimblast ffmpegthumbnailer tumbler playerctl             \
-noise-suppression-for-voice thunar-archive-plugin kitty       \
-waybar wlogout wlsunset sddm pamixer     \
-nwg-look-bin dunst ttf-firacode-nerd noto-fonts \
-noto-fonts-emoji ttf-nerd-fonts-symbols-common otf-firamono-nerd \
-brightnessctl hyprpicker hypridle hyprlock whitesur-gtk-theme\
-catppuccin-gtk-theme-mocha catppuccin-gtk-theme-macchiato catppuccin-gtk-theme-frappe catppuccin-gtk-theme-latte\
+
+git_pkgs="grimblast-git sddm-git waybar-git"
+   hypr_pkgs="hyprland wl-clipboard wf-recorder rofi wlogout hyprpicker hypridle hyprlock brightnessctl dunst swaybg kitty"    
+   font_pkgs="ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font otf-sora ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa"
+   font_pkgs2="ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts ttf-firacode-nerd"
+   app_pkgs="nwg-look-bin qt5ct btop jq gvfs ffmpegthumbs swww mousepad mpv  playerctl pamixer noise-suppression-for-voice"
+   app_pkgs2="polkit-gnome ffmpeg neovim viewnior pavucontrol thunar ffmpegthumbnailer tumbler thunar-archive-plugin xdg-user-dirs"
+   theme_pkgs="nordic-theme papirus-icon-theme starship whitesur-gtk-theme catppuccin-gtk-theme-mocha catppuccin-gtk-theme-macchiato catppuccin-gtk-theme-frappe catppuccin-gtk-theme-latte"
+
+
+
+    if ! yay -S --noconfirm $git_pkgs $hypr_pkgs $font_pkgs $font_pkgs2 $app_pkgs $app_pkgs2 $theme_pkgs 2>&1 | tee -a $LOG; then
+        print_error " Failed to install additional packages - please check the install.log \n"
+        exit 1
+    fi
+    xdg-user-dirs-update
+    echo
+    print_success " All necessary packages installed successfully."
+else
+    echo
+    print_error " Packages not installed - please check the install.log"
+    sleep 1
 fi
+
 
 ### Copy Config Files ###
 read -n1 -rep 'Would you like to copy config files? (y,n)' CFG
 if [[ $CFG == "Y" || $CFG == "y" ]]; then
     echo -e "Copying config files...\n"
-    cp -R ./dotconf/dunst ~/.config/
-    cp -R ./dotconf/hypr ~/.config/
-    cp -R ./dotconf/kitty ~/.config/
-    cp -R ./dotconf/neofetch ~/.config/
-    cp -R ./dotconf/pipewire ~/.config/
-    cp -R ./dotconf/rofi ~/.config/
-    cp -R ./dotconf/waybar ~/.config/
-    cp -R ./dotconf/wlogout ~/.config/
+    cp -R ./dotconf/dunst ~/.config/ 2>&1 | tee -a $LOG
+    cp -R ./dotconf/hypr ~/.config/ 2>&1 | tee -a $LOG
+    cp -R ./dotconf/kitty ~/.config/ 2>&1 | tee -a $LOG
+    cp -R ./dotconf/neofetch ~/.config/ 2>&1 | tee -a $LOG
+    cp -R ./dotconf/pipewire ~/.config/ 2>&1 | tee -a $LOG
+    cp -R ./dotconf/rofi ~/.config/ 2>&1 | tee -a $LOG
+    cp -R ./dotconf/waybar ~/.config/ 2>&1 | tee -a $LOG
+    cp -R ./dotconf/wlogout ~/.config/ 2>&1 | tee -a $LOG
 
     mkdir -p ~/Pictures/wallpaper
     cp -R ./wallpaper ~/Pictures/wallpaper/
@@ -68,14 +127,34 @@ if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
     sudo systemctl enable sddm
     sleep 3
 fi
+# BLUETOOTH
+read -n1 -rep "${CAT} OPTIONAL - Would you like to install Bluetooth packages? (y/n)" BLUETOOTH
+if [[ $BLUETOOTH =~ ^[Yy]$ ]]; then
+    printf " Installing Bluetooth Packages...\n"
+ blue_pkgs="bluez bluez-utils blueman"
+    if ! yay -S --noconfirm $blue_pkgs 2>&1 | tee -a $LOG; then
+       	print_error "Failed to install bluetooth packages - please check the install.log"    
+    printf " Activating Bluetooth Services...\n"
+    sudo systemctl enable --now bluetooth.service
+    sleep 2
+    fi
+else
+    printf "${YELLOW} No bluetooth packages installed..\n"
+	fi
 
+	fi
 
 ### Script is done ###
-echo -e "Script had completed.\n"
-echo -e "You can start Hyprland by typing Hyprland (note the capital H).\n"
-read -n1 -rep 'Would you like to start Hyprland now? (y,n)' HYP
-if [[ $HYP == "Y" || $HYP == "y" ]]; then
-    exec Hyprland
+printf "\n${GREEN} Installation Completed.\n"
+echo -e "${GREEN} You can start Hyprland by typing Hyprland (note the capital H).\n"
+read -n1 -rep "${CAT} Would you like to start Hyprland now? (y,n)" HYP
+if [[ $HYP =~ ^[Yy]$ ]]; then
+    if command -v Hyprland >/dev/null; then
+        exec Hyprland
+    else
+         print_error " Hyprland not found. Please make sure Hyprland is installed by checking install.log.\n"
+        exit 1
+    fi
 else
     exit
 fi
